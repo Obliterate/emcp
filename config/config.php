29c6efeaffaa270ce21124b578af5ed5cb65f257
obliteratev2 <obliteratev2@1d69db63-ff4f-4278-8916-0276e6e9ab2f>
2012-02-06 11:25:58 +0000

    renamed

---
+++ config/config.php
@@ -0,0 +1,36 @@
+<?php
+
+// MySQL Ragnarok Database
+
+'databaseconfig'	=> array(
+	'port'			=> 3306,
+	'hostname'		=> '127.0.0.1',
+	'username'		=> 'ragnarok',
+	'password'		=> 'ragnarok',
+	'database'		=> 'ragnarok',
+);
+
+// MySQL Log Database
+
+'ldatabaseconfig'	=> array(
+	'port'			=> 3306,
+	'hostname'		=> '127.0.0.1',
+	'username'		=> 'ragnarok',
+	'password'		=> 'ragnarok',
+	'database'		=> 'ragnarok',
+);
+
+// Server addresses for status display
+
+'Server'			=> array(
+	'loginaddress'	=> '127.0.0.1',
+	'charaddress'	=> '127.0.0.1',
+	'mapaddress'	=> '127.0.0.1',
+	'lPort'			=> 6900,
+	'cport'			=> 6121,
+	'mport'			=> 5121,
+	'UseMD5'		=> false,
+	'Database'		=> 'log'
+);
+
+?>
